8f8403815d6a046266bc7e0022d0f74dd3c03c6e
atindal97 <91355969+atindal97@users.noreply.github.com>
2021-10-01 18:22:39 -0700

    Initial commit

---
+++ Assets/Scripts/ToyBox/KillBox.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBox : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            PlayerReset.Instance?.ResetLastPosition();
+        }
+    }
+
+    private IEnumerator PlatformFallDelay()
+    {
+        yield return new WaitForSeconds(1f);
+
+    }
+}
